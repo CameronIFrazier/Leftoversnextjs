1bc364523dfb9347edb7a57dd385d9cfc7f45f66
CameronIFrazier <taterfrazier1@gmail.com>
2025-10-23 23:42:23 -0700

    working version for deliverable 10/23

--- src/app/SponsorPage/page.tsx
+++ src/app/SponsorPage/page.tsx
@@ -94,32 +94,7 @@ export default function SponsorPage() {
 
   return (
     <>
-      <h1>Our Sponsors</h1>
-
-      <p>
-        <strong>100 Treasures</strong> 100 Treasures is an LA-based esports & lifestyle brand founded
-        in 2017. This team typically competes across titles such as Call of Duty, Valorant, Apex Legens,
-        and etc. Most importantly 100 Treasures has won the 2022 Call of Duty League Championship.
-      </p>
-
-      <p>
-        <strong>G-Too Esports:</strong> G-Too Esports orginates from Berlin, germany with many teams in LoL, CS2,
-        Valorant, Rocket League, and many more. Their most decorated team comes from the Europrean League of Legends
-        team in the LEC. Their LEC team has went far even winning the LEC grand final in Madrid this fall.
-      </p>
-
-      <p>
-        <strong>Gee-Gee:</strong> Gee-Gee was founded in 2017 with bases in LA, Seoul, and Shanghai. This global
-        organization has its focus not only in League of Legends, but also smaller content creators. These content
-        creators help bridge a relationship between the U.S. and Asia.
-      </p>
-
-      <p>
-        <strong>LOUDER:</strong> LOUDER is a Brazilian organization founded in 2019 around São Paulo that are known
-        for their massive social reach built on a content-first model. One of their more notable wins are the VALORANT
-        world champions in 2022. Their dedicated fanbase supports this org across different games including Free Fire, CS2,
-        League of Legends, and many more.
-      </p>
+      
       <section className="w-full min-h-screen bg-black text-white flex flex-col items-center">
       <section className="w-[70%] pb-10 pt-8">
         {/* Header */}
