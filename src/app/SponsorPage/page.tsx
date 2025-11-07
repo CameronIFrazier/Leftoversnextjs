673c994d27bfb658fdda1318d6a113aadb0169e1
AnThuyNguyen <nguyen.ann.1411@gmail.com>
2025-11-07 05:11:33 -0800

    change layout, keep thing consistent

--- src/app/SponsorPage/page.tsx
+++ src/app/SponsorPage/page.tsx
@@ -171,25 +171,24 @@ export default function SponsorPage() {
   return (
     <section className="w-full min-h-screen bg-black text-white flex flex-col items-center pb-10 pt-8 w-[70%] px-10 py-10">
         {/* Header */}
-        <div className="sticky top-0 z-50 w-full bg-black/95 backdrop-blur-md border-b border-gray-700 flex px-6 items-center ">
-            <div className="sticky top-0 z-50 w-full bg-black/95 backdrop-blur-md border-b border-gray-700 flex px-6 items-center ">
-            <div className =  "flex flex-col">
-              <h1 className="text-2xl font-bold bg-gradient-to-r from-indigo-500 to-purple-500 inline-block pr-54 pl-4 sm:pr-20">Find Sponsor</h1>
-              <p className="text-sm text-gray-300">
-                Discover partners and organizations supporting our community.
-              </p> 
-            </div> 
-            <FloatingDockDemo />
-          <div>
+              <div className="sticky top-0 z-50 w-full bg-black/95 border-b border-gray-700 flex px-6 items-center justify-between mb-6">
+                <div className =  "flex flex-col">
+                  <h1 className="text-2xl font-bold bg-gradient-to-r from-indigo-500 to-purple-500 inline-block pr-54 pl-4 sm:pr-20">Find Sponsor</h1>
+                  <p className="text-sm text-gray-300">
+                    Discover partners and organizations supporting our community.
+                  </p> 
+                </div>  
+                          <div>
             <input
               value={query}
               onChange={(e) => setQuery(e.target.value)}
               placeholder="Search sponsors..."
               className="px-3 py-2 w-64 rounded-md border border-white bg-indigo-900 placeholder-gray-300 text-white"
             />
-          </div>
-        </div>
-        </div>
+          </div>   
+                <FloatingDockDemo />
+              </div>
+
         {/* Card Grid */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {filtered.length === 0 ? (
