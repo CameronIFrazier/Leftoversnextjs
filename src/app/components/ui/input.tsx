2f9aba14d6d83c6a37fa2f5347f6c91db597ccf5
CameronIFrazier <taterfrazier1@gmail.com>
2025-11-20 16:43:44 -0800

    moved lib

--- src/app/components/ui/input.tsx
+++ src/app/components/ui/input.tsx
@@ -1,7 +1,7 @@
 // Input component extends from shadcnui - https://ui.shadcn.com/docs/components/input
 "use client";
 import * as React from "react";
-import { cn } from "@/app/api/lib/utils";
+import { cn } from "@/lib/utils";
 import { useMotionTemplate, useMotionValue, motion } from "motion/react";
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>; {}
--- src/app/components/ui/shineborder.tsx
+++ src/app/components/ui/shineborder.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 
-import { cn } from "@/app/api/lib/utils"
+import { cn } from "@/lib/utils"
 
 interface ShineBorderProps extends React.HTMLAttributes<HTMLDivElement> {
   /**
