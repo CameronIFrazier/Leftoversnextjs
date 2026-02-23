88bec19d964fa2a1ac2a936e0d9946d7ad984312
CameronIFrazier <taterfrazier1@gmail.com>
2026-02-23 15:46:25 -0800

    update next.js, updated next.confog.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     domains: ["res.cloudinary.com"],
-  },
-  api: {
-    bodyParser: false, // ✅ disable body parser for file uploads
   },
 };
 
